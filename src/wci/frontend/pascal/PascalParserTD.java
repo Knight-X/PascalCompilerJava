07ab3556faae528d8f782b1e86bf30cfcf6d53ed
Kazami <kazamizero@gmail.com>
2014-11-27 23:38:06 +0800

    achieve the aprt of implementation of parse and scanner

---
+++ src/wci/frontend/pascal/PascalParserTD.java
@@ -0,0 +1,39 @@
+package wci.frontend.pascal;
+
+import wci.frontend.*;
+import wci.message.Message;
+
+import static wci.message.MessageType.PARSER_SUMMARY;
+
+public class PascalParserTD extends Parser
+{
+  public PascalParserTD(Scanner scanner)
+  {
+    supper(scanner);
+  }
+
+  public void parse() 
+    throws Exception
+  {
+    Token token;
+    long startTime = System.currentTimeMillis();
+
+    while (!((token = nextToken()) instanceof EofToken)) {}
+
+     float elapsedTime = (System.currentTimeMillis() - startTime ) / 1000f;
+
+     sendMessage(new Message(PARSER_SUMMARY,
+			new Number[] {token.getLineNumber(),
+			              getErrorCount(),
+                                      elapsedTime}));
+    }
+
+
+    public int getErrorCount()
+    {
+      return 0;
+    }
+
+}
+      
+    
