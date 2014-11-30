f98254b512659805003c3157208d94216b4ce48d
Kazami <kazamizero@gmail.com>
2014-11-30 20:45:19 +0800

    achieve the parserTD partially

--- src/wci/frontend/pascal/PascalParserTD.java
+++ src/wci/frontend/pascal/PascalParserTD.java
@@ -3,10 +3,16 @@ import wci.frontend.*;
 import wci.message.Message;
 import wci.frontend.Parser;
 
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
+
 import static wci.message.MessageType.PARSER_SUMMARY;
+import static wci.message.MessageType.*;
 
 public class PascalParserTD extends Parser
 {
+  protected static PascalErrorHandler errorHandler = new PascalErrorHandler();
+
   public PascalParserTD(Scanner scanner)
   {
     super(scanner);
@@ -18,7 +24,23 @@ public class PascalParserTD extends Parser
     Token token;
     long startTime = System.currentTimeMillis();
 
-    while (!((token = nextToken()) instanceof EofToken)) {}
+    try{
+      while (!((token = nextToken()) instanceof EofToken)) {
+        TokenType tokenType = token.getType();
+
+        if (tokenType != ERROR) {
+          sendMessage(new Message(TOKEN,
+                        new Object[] {token.getLineNumber(),
+                                      token.getPosition(),
+                                      tokenType,
+                                      token.getText(),
+                                      token.getValue()}));
+        } else {
+           errorHandler.flag(token, (PascalErrorCode)token.getValue, this);
+        }
+
+        
+      }
 
      float elapsedTime = (System.currentTimeMillis() - startTime ) / 1000f;
 
@@ -26,12 +48,16 @@ public class PascalParserTD extends Parser
 			new Number[] {token.getLineNumber(),
 			              getErrorCount(),
                                       elapsedTime}));
+      }
+     catch (java.io.IOException ex) {
+       errorHandler.abortTranslation(IO_ERROR, this);
+     }
     }
 
 
     public int getErrorCount()
     {
-      return 0;
+      return errorHandler.getErrorCount();
     }
 
 }
