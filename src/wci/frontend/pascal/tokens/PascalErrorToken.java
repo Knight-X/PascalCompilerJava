fd971cbfdc2a87777954558eb14a7377eb54e7bb
Kazami <kazamizero@gmail.com>
2014-12-02 23:34:42 +0800

    achieve the pascalerrortoken.java

---
+++ src/wci/frontend/pascal/tokens/PascalErrorToken.java
@@ -0,0 +1,26 @@
+package wci.frontend.pascal.tokens;
+
+import wci.frontend.*;
+import wci.frontend.pascal.*;
+
+
+import static wci.frontend.pascal.PascalTokenType.*;
+
+public class PascalErrorToken extends PascalToken
+{
+  public PascalErrorToken(Source source, PascalErrorCode erroCode,
+			String tokenText)
+    throws Exception
+  {
+    super(source);
+
+    this.text = tokenText;
+    this.type = ERROR;
+    this.value = errorCode;
+  }
+
+  protected void extract()
+    throws Exception
+  {
+  }
+}
