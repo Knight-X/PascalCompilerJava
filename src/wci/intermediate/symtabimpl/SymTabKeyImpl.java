dca59e1438489bf8f9ff0efb2cef9f053442083f
Kazami <kazamizero@gmail.com>
2014-12-06 18:22:05 +0800

    achieve the enum class and some implement of the symtab

---
+++ src/wci/intermediate/symtabimpl/SymTabKeyImpl.java
@@ -0,0 +1,16 @@
+package wci.intermediate.symtabimpl;
+
+import wci.intermediate.SymTabKey;
+
+
+public enum SymTabKeyImpl implements SymTabKey
+{
+  CONSTANT_VALUE,
+
+  ROUTINE_CODE, ROUTINE_SYMTAB, ROUTINE_ICODE,
+ 
+  ROUTINE_PARMS, ROUTINE_ROUTINES,
+
+  DATA_VALUE,
+
+} 
---
+++ tmp/HH2.java
@@ -0,0 +1,17 @@
+public enum HH2
+{
+  CONSTANT_VALUE,
+  ROUTINE_CODE,
+
+  DATA_VALUE;
+
+
+  public static void main(String[] args){
+
+  for (HH2 p : HH2.values()) {
+    System.out.printf("%s\n", p);
+  }
+}
+}
+
+  
