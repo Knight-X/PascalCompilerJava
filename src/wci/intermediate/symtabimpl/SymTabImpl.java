b409a6e82fbed65695cbaaf64d0e3ca0376335fd
Kazami <kazamizero@gmail.com>
2014-12-06 18:07:36 +0800

    achieve the SymTabImpl

---
+++ src/wci/intermediate/symtabimpl/SymTabImpl.java
@@ -0,0 +1,53 @@
+package wci.intermediate.symtabimpl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+
+import wci.intermediate.*;
+
+public class SymTabImpl
+  extends TreeMap<String, SymTabEntry>
+  implements SymTab
+{
+  private int nestingLevel;
+
+  public SymTabImpl(int nestingLevel)
+  {
+    this.nestingLevel = nestingLevel;
+  }
+
+  public int getNestingLevel()
+  {
+    return nestingLevel;
+  }
+
+  public SymTabEntry enter(String name)
+  {
+    SymTabEntry entry = SymTabFactory.createSymTabEntry(name, this);
+
+    put(name, entry);
+    return entry;
+  }
+
+  public SymTabEntry lookup(String name)
+  {
+    return get(name);
+  }
+
+  public ArrayList<SymTabEntry> sortedEntries()
+  {
+    Collection<SymTabEntry> entries = values();
+
+    Interator<SymTabEntry> iter = entries.iterator();
+    ArrayList<SymTabEntry> list = new ArrayList<SymTabEntry>(size());
+
+    while (iter.hasNext()) {
+      list.add(iter.next());
+    }
+
+    return list;
+  }
+}
