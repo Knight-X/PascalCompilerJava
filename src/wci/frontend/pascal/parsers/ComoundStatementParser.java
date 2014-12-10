d8e196f795d2f2714273055192c711776fe52a9c
Kazami <kazamizero@gmail.com>
2014-12-10 22:26:53 +0800

    complete the compoundstatementparser.java

---
+++ src/wci/frontend/pascal/parsers/ComoundStatementParser.java
@@ -0,0 +1,31 @@
+package wci.frontend.pascal.parsers;
+
+import wci.frontend.*;
+import wci.frontend.pascal.*;
+import wci.intermediate.*;
+
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.*;
+
+public class CompoundStatementParser extends StatementParser
+{
+  
+  public CompoundStatementParser(PascalParserTD parent)
+  {
+    super(parent);
+  }
+
+  public ICodeNode parse(Token token)
+  {
+    token = nextToken();
+
+    ICodeNode compoundNode = ICodeFactory.createICodeNode(COMPOUND);
+
+    StatementParser statementParser = new StatementParser(this);
+
+    statementParser.parseList(token, compoundNode, END, MISSING_END);
+
+    return compoundNode;
+  }
+}
