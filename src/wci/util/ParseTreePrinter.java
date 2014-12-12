20d4750e87f2a3f3d442738f297223f193f262e8
Kazami <kazamizero@gmail.com>
2014-12-13 00:00:27 +0800

    start the parsetreeprinter

---
+++ src/wci/util/ParseTreePrinter.java
@@ -0,0 +1,13 @@
+package wci.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.io.PrintStream;
+
+import wci.intermediate.*;
+import wci.intermediate.icodeimpl.*;
+
+
+
