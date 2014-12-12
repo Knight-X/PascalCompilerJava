07a8260c50a6495d479934d6af91b0fd9ddafb56
Kazami <kazamizero@gmail.com>
2014-12-13 00:05:20 +0800

    achieve parf of the parsetreeprinter

--- src/wci/util/ParseTreePrinter.java
+++ src/wci/util/ParseTreePrinter.java
@@ -10,4 +10,27 @@ import wci.intermediate.*;
 import wci.intermediate.icodeimpl.*;
 
 
+public class ParseTreePrinter
+{
+  private static final int INDENT_WIDTH = 4;
+  private static final int LINE_WIDTH = 80;
 
+  private PrintStream ps;
+  private int length;
+  private String indent;
+  private String indentation;
+  private StringBuilder line;
+
+  public ParseTreePrinter(PrintStream ps)
+  {
+    this.ps = ps;
+    this.length = 0;
+    this.indentation = "";
+
+    this.line = new StringBuilder();
+    this.indent = "";
+
+    for (int i = 0; i < INDENT_WIDTH; ++i) {
+      this.indent += " ";
+    }
+  }
