10b6522180f925fefb8443a4c315a7b30b62a3e1
Kazami <kazamizero@gmail.com>
2014-12-13 21:40:32 +0800

    achieve the expressionexecutor

--- src/wci/backend/interpreter/executors/ExpressionExecutor.java
+++ src/wci/backend/interpreter/executors/ExpressionExecutor.java
@@ -77,5 +77,115 @@ public class ExpressionExecutor extends StatementExecutor
     ICodeNode operandNode1 = children.get(0);
     ICodeNode operandNode2 = children.get(1);
 
+    Object operand1 = execute(operandNode1);
+    Object operand2 = execute(operandNode2);
 
+    boolean integerMode = (operand1 instanceof Integer) && (operand2 instanceof Integer);
+
+    if (ARITH_OPS.contains(nodeType)) {
+      if (integerMode) {
+        int value1 = (Integer) operand1;
+        int value2 = (Integer)operand2;
+
+        switch (nodeType) {
+          case ADD: return value1 + value2;
+          case SUBTRACT: return value1 - value2;
+          case MULTIPLY: return value1 * value2;
+
+          case FLOAT_DIVIDE: {
+
+            if (value2 != 0) {
+              return ((float) value1)/((float) value2);
+
+            } else {
+              errorHandler.flag(node, DIVISION_BY_ZERO, this);
+              return 0;
+            }
+
+          }
+
+          case INTEGER_DIVIDE: {
+            if (value2 != 0) {
+              return value1/value2;
+            } else {
+              errorHandler.flag(node, DIVISION_BY_ZERO, this);
+              return 0;
+            }
+
+          }
+
+          case MOD: {
+     
+            if (value2 != 0) {
+              return value1%value2;
+            } else {
+              errorHandler.flag(node, DIVISION_BY_ZERO, this);
+              return 0;
+            }
+          }
+        }
+      }
+      else {
+        float value1 = operand1 instanceof Integer ? (Integer) operand1 : (Float) operand1;
+        float value2 = operand2 instanceof Integer ? (Integer) operand2 : (Float) operand2;
+
+        switch (nodeType) {
+          case ADD: return value1 + value2;
+          case SUBTRACT: return value1 - value2;
+          case MULTIPLY: return value1 * value2;
+
+          case FLOAT_DIVIDE: {
+
+            if (value2 != 0.0f) {
+              return value1/value2;
+            } else {
+              errorHandler.flag(node, DIVISION_BY_ZERO, this);
+              return 0.0f;
+            }
+
+          }
+        }
+      }
+    }
+
+    else if ((nodeType == AND) || (nodeType == OR)) {
+      boolean value1 = (Boolean) operand1;
+      boolean value2 = (Boolean) operand2;
+
+      switch (nodeType) {
+         case AND: return value1 && value2;
+         case OR: return value1 || value2;
+      }
+    }
+
+    else if (integerMode) {
+      int value1 = (Integer) operand1;
+      int value2 = (Integer) operand2;
+
+      switch (nodeType) {
+        case EQ: return value1 == value2;
+        case NE: return value1 != value2;
+        case LT: return value1 < value2;
+        case LE: return value1 <= value2;
+        case GT: return value1 > value2;
+        case GE: return value1 >= value2;
+      }
+    }
+    else {
+       float value1 = operand1 instanceof Integer ? (Integer) operand1 : (Float) operand1;
+       float value2 = oeprand2 instanceof Integer ? (Integer) operand2 : (Float) oeprand2;
+
+       switch (nodeType) {
+         case EQ: return value1 == value2;
+         case NE: return value1 != value2;
+         case LT: return value1 < value2;
+         case LE: return value1 <= value2;
+         case GT: return value1 > value2;
+         case GE: return value1 >= value2;
+       }
+     }
+
+     return 0;
+  }
+}
       
