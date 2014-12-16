5b0e28ff1c183ffb75a8844edc2cf3d700f23e0e
Kazami <kazamizero@gmail.com>
2014-12-16 21:24:19 +0800

    achieve the sudo code for the whilestatementparser

---
+++ src/wci/frontend/pascal/parsers/WhileStatementParser.java
@@ -0,0 +1,63 @@
+package wci.frontend.pascal.parsers;
+
+import java.util.EnumSet;
+
+
+import wci.frontend.*;
+import wci.frontend.pascal.*;
+import wci.intermediate.*;
+import wci.intermediate.icodeimpl.*;
+
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.*;
+import static wci.intermediate.icodeimpl.ICodeKeyImpl.*;
+
+
+public class WhileStatementParser extends StatementParser
+{
+
+  public WhileStatementParser(PascalParserTD parent)
+  {
+    super(parent);
+  }
+
+
+  private static final EnumSet<PascalTokenType> DO_SET =
+    StatementParser.STMT_START_SET.clone();
+
+  static {
+    DO_SET.add(DO);
+    DO_SET.addAll(StatementParser.STMT_FOLLOW_SET);
+  }
+
+  public ICodeNode parse(Token token)
+    throws Exception
+  {
+    token = nextToken();
+
+    ICodeNode loopNode = ICodeFactory.createICodeNode(LOOP);
+    ICodeNode breakNode = ICodeFactory.createICodeNode(TEST);
+    ICodeNode notNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.NOT);
+
+    loopNode.addChild(breakNode);
+    breakNode.addChild(notNode);
+
+    ExpressionParser expressionParser = new ExpressionParser(this);
+
+    notNode.addChild(expressionParser.parse(token));
+
+    token = synchronize(DO_SET);
+
+    if (token.getType() == DO) {
+      token = nextToken();
+    } else {
+      errorHanlder.flag(token, MISSING_DO, this);
+    }
+
+    StatementParser statementParser = new StatementParser(this);
+    loopNode.addChild(StatementParser.parse(token));
+
+    return loopNode;
+  }
+}
