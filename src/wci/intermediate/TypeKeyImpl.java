4a79e6ded27de62ed01991fbaa94f94065581a5a
Kazami <kazamizero@gmail.com>
2015-01-03 15:40:37 +0800

    typkeyimpl

---
+++ src/wci/intermediate/TypeKeyImpl.java
@@ -0,0 +1,14 @@
+package wci.intermediate.typeimpl;
+
+import wci.intermediate.TypeKey;
+
+public enum TypeKeyImpl extends TypeKey
+{
+    ENUMERATION_CONSTANTS,
+
+    SUBRANGE_BASE_TYPE, SUBRANGE_MIN_VALUE, SUBRANGE_MAX_VALUE,
+
+    ARRAY_INDEN_TYPE, ARRAY_ELEMENT_TYPE, ARRAY_ELEMENT_COUNT,
+
+    RECORD_SYMTAB
+}
