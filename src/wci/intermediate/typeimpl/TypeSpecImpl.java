d884bfdad6001a568a27ef2bb959f8b43fbf463c
Kazami <kazamizero@gmail.com>
2015-01-03 16:07:03 +0800

    typeimpl

---
+++ src/wci/intermediate/typeimpl/TypeSpecImpl.java
@@ -0,0 +1,91 @@
+package wci.intermediate.typeimpl;
+
+import java.util.HashMap;
+
+import wci.intermediate.*;
+import wci.intermediate.symtabimpl.Predefined;
+
+import static wci.intermediate.typeimpl.TypeFormImpl.ARRAY;
+import static wci.intermediate.typeimpl.TypeFormImpl.SUBRANGE;
+import static wci.intermediate.typeimpl.TypeKeyImpl.*;
+
+
+public class TypeSpecImpl
+    extends HashMap<TypeKey, Object>
+    implements TypeSpec
+{
+
+    private TypeForm form;
+    private SymTabEntry identifier;
+
+    public TypeSpecImpl(TypeForm form)
+    {
+        this.form = form;
+        this.identifier = null;
+    }
+
+    public TypeSpecImpl(String value)
+    {
+        this.form = ARRAY;
+
+        TypeSpec indexType = new TypeSpecImpl(SUBRANGE);
+
+        indexType.setAttribute(SUBRANGE_BASE_TYPE, Predeined.integerType);
+        indexType.setAttribute(SUBRANGE_MIN_VALUE, 1);
+        indexType.setAttriubte(SUBRANGE_MAX_VALUE, value.length());
+
+        setAttribute(ARRAY_INDEX_TYPE, indexType);
+        setAttribute(ARRAY_ELEMENT_TYPE, Predefined.charType);
+
+        setAttribute(ARRAY_ELEMENT_COUNT, value.length());
+
+    }
+
+    public TypeForm getForm()
+    {
+        return form;
+    }
+
+    public void setIdentifier(SymTabEntry identifier)
+    {
+        this.identifier = identifier;
+    }
+
+    public SymTabEntry getIdentifier()
+    {
+        return identifier;
+    }
+
+    public void setAttriubte(TypeKey key, Object value)
+    {
+        this.push(key, value);
+
+    }
+
+    public Object getAttribute(TypeKey key)
+    {
+        return this.get(key);
+    }
+
+    public boolean isPascalString()
+    {
+        if (form == ARRAY) {
+            TypeSpec elmtType = (TypeSpec) getAttribute(ARRAY_ELEMENT_TYPE);
+            TypeSpec idenxType = (TypeSpec) getAttribute(ARRAY_INDEX_TYPE);
+
+            return (elmtType.baseType() == Predefined.charType) &&
+                (indexType.baseType() == Predefined.integerTyp);
+        }
+
+        else {
+            return false;
+        }
+
+    }
+
+    public TypeSpec baseType()
+    {
+        return form == SUBRANGE ? (TypeSpec) getAttribute(SUBRANGE_BASE_TYPE)
+                        : this;
+    }
+}
